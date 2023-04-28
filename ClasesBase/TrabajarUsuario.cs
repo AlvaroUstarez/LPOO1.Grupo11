849d4fd367c45d932cafdb25b718de736e0c4060
BM-NT-2018-0025\LPOO <ale9622@gmail.com>
2023-04-27 23:32:23 -0300

    Listar Roles

---
+++ ClasesBase/TrabajarUsuario.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ClasesBase
+{
+    class TrabajarUsuario
+    {
+        public static DataTable list_roles(){
+            SqlConnection cnn = new SqlConnection(ClasesBase.Properties.Settings.Default.opticaConnectionString);
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "Select * From roles";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = cnn;
+
+            //Ejecuta la consulta
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+
+            DataTable dt = new DataTable();
+
+            //Llena los datos de la consulta en el DataTable
+            da.Fill(dt);
+
+            return dt;
+        }
+    }
+}
