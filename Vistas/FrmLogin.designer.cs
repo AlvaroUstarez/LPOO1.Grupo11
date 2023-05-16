30160ca10daaf20383b9882dce80910af88c8574
Alvaro Ustrarez <ale9622@gmail.com>
2023-05-16 16:18:17 -0300

    Vamo ñubel.. desarrollo ok!

--- Vistas/FrmLogin.designer.cs
+++ Vistas/FrmLogin.designer.cs
@@ -28,104 +28,88 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmLogin));
-            this.lblUsuario = new System.Windows.Forms.Label();
-            this.lblPassword = new System.Windows.Forms.Label();
-            this.btnAceptarLogin = new System.Windows.Forms.Button();
-            this.lblBienvenida = new System.Windows.Forms.Label();
-            this.txtUsuario = new System.Windows.Forms.TextBox();
-            this.txtPass = new System.Windows.Forms.TextBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // lblUsuario
+            // button1
             // 
-            this.lblUsuario.AutoSize = true;
-            this.lblUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblUsuario.Location = new System.Drawing.Point(26, 173);
-            this.lblUsuario.Name = "lblUsuario";
-            this.lblUsuario.Size = new System.Drawing.Size(72, 22);
-            this.lblUsuario.TabIndex = 0;
-            this.lblUsuario.Text = "Usuario";
+            this.button1.Location = new System.Drawing.Point(42, 186);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
-            // lblPassword
+            // button2
             // 
-            this.lblPassword.AutoSize = true;
-            this.lblPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblPassword.Location = new System.Drawing.Point(26, 233);
-            this.lblPassword.Name = "lblPassword";
-            this.lblPassword.Size = new System.Drawing.Size(103, 22);
-            this.lblPassword.TabIndex = 1;
-            this.lblPassword.Text = "Contraseña";
+            this.button2.Location = new System.Drawing.Point(178, 186);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
             // 
-            // btnAceptarLogin
+            // label1
             // 
-            this.btnAceptarLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAceptarLogin.Location = new System.Drawing.Point(118, 292);
-            this.btnAceptarLogin.Name = "btnAceptarLogin";
-            this.btnAceptarLogin.Size = new System.Drawing.Size(135, 30);
-            this.btnAceptarLogin.TabIndex = 2;
-            this.btnAceptarLogin.Text = "Aceptar";
-            this.btnAceptarLogin.UseVisualStyleBackColor = true;
-            this.btnAceptarLogin.Click += new System.EventHandler(this.btnAceptarLogin_Click);
-            this.btnAceptarLogin.MouseLeave += new System.EventHandler(this.btnAceptarLogin_MouseLeave);
-            this.btnAceptarLogin.MouseHover += new System.EventHandler(this.btnAceptarLogin_MouseHover);
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Usuario";
             // 
-            // lblBienvenida
+            // label2
             // 
-            this.lblBienvenida.AutoSize = true;
-            this.lblBienvenida.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblBienvenida.Location = new System.Drawing.Point(25, 26);
-            this.lblBienvenida.Name = "lblBienvenida";
-            this.lblBienvenida.Size = new System.Drawing.Size(321, 31);
-            this.lblBienvenida.TabIndex = 5;
-            this.lblBienvenida.Text = "Bienvenido - Inicie sesión";
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Contraseña";
             // 
-            // txtUsuario
+            // textBox1
             // 
-            this.txtUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtUsuario.Location = new System.Drawing.Point(185, 173);
-            this.txtUsuario.Name = "txtUsuario";
-            this.txtUsuario.Size = new System.Drawing.Size(161, 27);
-            this.txtUsuario.TabIndex = 6;
+            this.textBox1.Location = new System.Drawing.Point(100, 63);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(153, 20);
+            this.textBox1.TabIndex = 4;
             // 
-            // txtPass
+            // textBox2
             // 
-            this.txtPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtPass.Location = new System.Drawing.Point(185, 233);
-            this.txtPass.Name = "txtPass";
-            this.txtPass.PasswordChar = 'x';
-            this.txtPass.Size = new System.Drawing.Size(161, 27);
-            this.txtPass.TabIndex = 7;
-            this.txtPass.UseSystemPasswordChar = true;
+            this.textBox2.Location = new System.Drawing.Point(100, 100);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(153, 20);
+            this.textBox2.TabIndex = 5;
             // 
-            // pictureBox1
+            // label3
             // 
-            this.pictureBox1.ErrorImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.ErrorImage")));
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(140, 60);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(91, 96);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 8;
-            this.pictureBox1.TabStop = false;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(121, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Bienvenido";
             // 
             // FrmLogin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(371, 353);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.txtPass);
-            this.Controls.Add(this.txtUsuario);
-            this.Controls.Add(this.lblBienvenida);
-            this.Controls.Add(this.btnAceptarLogin);
-            this.Controls.Add(this.lblPassword);
-            this.Controls.Add(this.lblUsuario);
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Name = "FrmLogin";
-            this.Text = "FrmLogin";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.Text = "Login";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -133,12 +117,12 @@
 
         #endregion
 
-        private System.Windows.Forms.Label lblUsuario;
-        private System.Windows.Forms.Label lblPassword;
-        private System.Windows.Forms.Button btnAceptarLogin;
-        private System.Windows.Forms.Label lblBienvenida;
-        private System.Windows.Forms.TextBox txtUsuario;
-        private System.Windows.Forms.TextBox txtPass;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
     }
 }
